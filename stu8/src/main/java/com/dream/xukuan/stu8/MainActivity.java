2388e56842d377d6d04a4d61641b8bc9f4581a31
DespK <danzihappy@163.com>
2018-02-24 18:32:52 +0800

    stu8

--- stu8/src/main/java/com/dream/xukuan/stu8/MainActivity.java
+++ stu8/src/main/java/com/dream/xukuan/stu8/MainActivity.java
@@ -2,16 +2,25 @@ package com.dream.xukuan.stu8;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 /**
  * @author xk
  */
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        listView = (ListView) findViewById(R.id.list_view);
 
     }
 }
