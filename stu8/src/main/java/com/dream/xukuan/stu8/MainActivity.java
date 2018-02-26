2704cf3f70df83d318860049c2ddc36d17cf1cce
DespK <danzihappy@163.com>
2018-02-26 18:22:38 +0800

    stu8 complete

--- stu8/src/main/java/com/dream/xukuan/stu8/MainActivity.java
+++ stu8/src/main/java/com/dream/xukuan/stu8/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.AbsListView;
 import android.widget.ListView;
+import android.widget.RadioGroup;
 
 import com.dream.xukuan.stu8.adapter.MyAdapter;
 import com.dream.xukuan.stu8.bean.CityEntity;
@@ -21,9 +22,10 @@ public class MainActivity extends AppCompatActivity {
     ListView listView;
     MyAdapter adapter;
     private int page = 1;
-    private int cityId = 1;
+    private static int cityId = 1;
     List<CityEntity> cityEntityList;
     private boolean isBottom;
+    RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void initViews() {
         listView = (ListView) findViewById(R.id.list_view);
+        radioGroup = (RadioGroup) findViewById(R.id.radiogroup);
         cityEntityList = new ArrayList<>();
         adapter = new MyAdapter(this,R.layout.item_layout,cityEntityList);
         listView.setAdapter(adapter);
@@ -64,6 +67,30 @@ public class MainActivity extends AppCompatActivity {
                     isBottom =true;
                 }else {
                     isBottom =false;
+                }
+            }
+        });
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.beiJing:{
+                        cityId = 1;
+                        startLoadTask();
+                    }
+                    break;
+                    case R.id.shangHai:{
+                        cityId = 2;
+                        startLoadTask();
+                    }
+                    break;
+                    case R.id.shenZheng:{
+                        cityId = 3;
+                        startLoadTask();
+                    }
+                    break;
+                    default:
+
                 }
             }
         });
