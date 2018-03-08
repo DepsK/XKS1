0bddc355c32e59ce67e2a12047c59b05902574ff
DespK <danzihappy@163.com>
2018-03-08 17:55:05 +0800

    stu15

---
+++ stu15/src/main/java/com/dream/xukuan/stu15/FirstActivity.java
@@ -0,0 +1,18 @@
+package com.dream.xukuan.stu15;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+        FragmentManager fg = getSupportFragmentManager();
+        FragmentTransaction transaction = fg.beginTransaction();
+
+    }
+}
