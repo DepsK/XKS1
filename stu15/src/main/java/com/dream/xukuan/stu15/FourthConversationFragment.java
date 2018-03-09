82b7d01d55eb22329e7b9f6d0bcbd64be399778b
DespK <danzihappy@163.com>
2018-03-09 17:29:14 +0800

    stu15 complete

---
+++ stu15/src/main/java/com/dream/xukuan/stu15/FourthConversationFragment.java
@@ -0,0 +1,21 @@
+package com.dream.xukuan.stu15;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author XK
+ * @date 2018/3/9.
+ */
+public class FourthConversationFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_fourth_conversationfragment,null);
+    }
+}
--- stu15/src/main/java/com/dream/xukuan/stu15/HWActivity.java
+++ stu15/src/main/java/com/dream/xukuan/stu15/HWActivity.java
@@ -1,13 +1,69 @@
 package com.dream.xukuan.stu15;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class HWActivity extends AppCompatActivity {
+
+    RadioGroup radioGroup;
+    Fragment[] fragments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hw);
+        radioGroup = (RadioGroup) findViewById(R.id.hw_radio);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton button = (RadioButton) group.findViewById(checkedId);
+                int index = Integer.parseInt(button.getTag().toString());
+                addFragment(index);
+            }
+        });
+        fragments = new Fragment[3];
+        addFragment(0);
+    }
+
+    int lastIndex = -1;
+    private void addFragment(int tag) {
+        if(lastIndex == tag){
+            return;
+        }
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        //如果之前存在其他的fragment,需要隐藏之前的fragment
+        if(lastIndex>0){
+            //transaction.hide(fragments[lastIndex]);//隐藏上次的fragment
+           //transaction.remove(fragments[lastIndex]);//移除
+            transaction.detach(fragments[lastIndex]);
+        }
+        if(fragments[tag]==null){
+            switch (tag){
+                case 0:{
+                    fragments[tag] = new FourthConversationFragment();
+                }
+                break;
+                case 1:{
+                    fragments[tag] = new FourthContactFragment();
+                }
+                break;
+                case 2:{
+                    fragments[tag] = new FourthPluginFragment();
+                }
+                break;
+                default:
+            }
+            transaction.add(R.id.hw_fragment_container,fragments[tag]);
+        }else {
+            transaction.attach(fragments[tag]);
+        }
+        transaction.commit();
+        lastIndex =tag;
     }
 }
